e35621ef07228c3e894b33bf841852b0efb8cec4
cmrohityadav <cmrohityadav23@gmail.com>
2025-01-26 18:14:14 +0530

    added order model

---
+++ server/models/order.model.js
@@ -0,0 +1,43 @@
+import mongoose,{Schema} from "mongoose";
+
+const itemSchema=new Schema({
+    product:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Product",
+        required:true,
+    },
+    quatity:{
+        type:Number,
+        required:true
+    }
+    
+},{timestamps:true})
+
+
+const orderSchema=new Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+    },
+    deliveryDate:{
+        type:String,
+    },
+    items:{
+        type:[itemSchema],
+        required:true
+    },
+    status:{
+        type:String,
+        enum:[
+            "Order Placed",
+            "Shipping",
+            "Delivered",
+            "cancelled"
+        ],
+        default:"Order Placed",
+        required:true
+    },
+
+},{timestamps:true})
+export const Order=mongoose.model("Order",productSchema)
