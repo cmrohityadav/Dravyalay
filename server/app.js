261c0d7346fece009c6c359d1c011ac883e25381
cmrohityadav <cmrohityadav23@gmail.com>
2025-01-26 09:20:25 +0530

    added basic routing to app.js

--- server/app.js
+++ server/app.js
@@ -7,14 +7,22 @@ const app=express();
 app.use(express.json());
 
 
+//////// IMPORT ROUTER /////////////
 
+import userRouter from "./routes/user.Router.js"
+app.use('/api/v1/user',userRouter)
 
 
 //initial routes for testing
 app.use('/ping',(req,res)=>{res.send("pong")})
+app.use('/user',userRouter)
 
+
+//// !!!!!IMPORTANT use it always last /////////////////
 app.all("*",(req,res)=>{
     res.status(404).send("OOPS !! 404 page not found")
 })
+
+
 
 export {app}
